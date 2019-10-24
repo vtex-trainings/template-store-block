03237e86cfb39060a0347ef419963c05f425f51f
Sávio Muniz <savio.muniz@vtex.com.br>
2019-10-23 20:32:40 -0400

    Node resolvers

--- node/resolvers/gif.ts
+++ node/resolvers/gif.ts
@@ -1,5 +1,5 @@
 export const gif = async (
     _: any,
-    {  }: ?,
-    { clients: { ? }}: Context
-) => null
+    { query }: { query: string},
+    { clients: { giphy }}: Context
+) => giphy.translateGif(query)
